885ee4618092fdbfc4d805c65bf2a4da9cef22d9
Rafael Consoleti <rafa.consoletti@gmail.com>
2025-07-20 17:48:29 -0300

    Primeiro commit com as bibliotecas que serão  utilizadas no código

---
+++ finanças.py
@@ -0,0 +1,4 @@
+import streamlit as st # cria a interface com streamlit
+import pandas as pd # tratamentos de dados 
+from db import inserir_trasacao, listar_transacoes # funções de acesso para o mysql
+
